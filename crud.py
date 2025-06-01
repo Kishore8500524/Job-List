d6349fa925d38c5e4b0c4c944f0fa3996e0859ee
Kishore8500524 <kishorechendi@gmail.com>
2025-06-01 17:38:13 +0530

    Add files via upload

--- crud.py
+++ crud.py
@@ -1,19 +1,20 @@
 import pandas as pd
 from sqlalchemy import text
 
-# Correct function name to match import in Job_Portal.py
 def get_all_jobs(engine):
+    """Fetch all job listings from the Jobs table."""
     return pd.read_sql("SELECT * FROM Jobs", engine)
 
 def post_job(engine, title, company, location, description):
+    """Insert a new job into the Jobs table."""
     with engine.begin() as conn:
         conn.execute(
             text("INSERT INTO Jobs (Title, Company, Location, Description) VALUES (:title, :company, :location, :desc)"),
             {"title": title, "company": company, "location": location, "desc": description}
         )
 
-# Correct name to match 'apply_to_job' import
 def apply_to_job(engine, job_id, applicant_name, email, resume):
+    """Insert an application into the Applications table."""
     with engine.begin() as conn:
         conn.execute(
             text("INSERT INTO Applications (JobID, ApplicantName, Email, ResumeText) VALUES (:job_id, :name, :email, :resume)"),
